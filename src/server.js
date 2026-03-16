47f34c96d380ca57948f985ce81c9abaf582e0ed
Vadym <vadimkiricenko572@gmail.com>
2026-03-16 17:37:22 +0200

    added

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(pino());
+
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Retrieved all notes",
+  });
+});
+
+app.get("/notes/:noteId", (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
