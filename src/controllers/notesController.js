0b16663b97612bc675f58c722e2ae9d3de61e05b
Vadym <vadimkiricenko572@gmail.com>
2026-03-23 17:37:09 +0200

    fix2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import {Note} from '../models/note.js';
 import createHttpError from 'http-errors';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
@@ -11,8 +11,9 @@ export const getNoteById = async (req, res) => {
   const note = await Note.findById(noteId);
 
   if (!note) {
-    return res.status(404).json({ message: 'Note not found' });
-  }
+      throw createHttpError(404, 'Note not found');
+    }
+
 
   res.status(200).json(note);
 };
@@ -42,7 +43,7 @@ export const updateNote = async (req, res) => {
     noteId,
     updateData,
     {
-      new: true,  
+      returnDocument: 'after',
       runValidators: true 
     }
   );
