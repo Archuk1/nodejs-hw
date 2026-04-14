e302f18a189e1f37393c6992f6ac0106d21c8af3
Vadym <vadimkiricenko572@gmail.com>
2026-04-14 16:54:28 +0300

    add

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import notesRouters from './routes/notesRoutes.js';
 import { errors } from "celebrate";
 import cookieParser from "cookie-parser";
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRouters);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
